cc4d89777ee7e0b6f046078a1f33d0326758de99
alchamist <alchamist@0c2b2200-50f8-11de-96a6-a1d7c9ea8392>
2009-06-06 17:35:39 +0000

    Started work on the tools page

--- trunk/includes/opensim.php
+++ trunk/includes/opensim.php
@@ -18,6 +18,7 @@ class OpenSim
   public $region_count;
   public $online_count;
   public $unique_count;
+  public $null_key = "00000000-0000-0000-0000-000000000000";
 
   function __construct() {
     require("settings.php");
@@ -244,6 +245,18 @@ class OpenSim
     return $string;
   }
 
+  public function createUUID($prefix = '')
+
+  {
+    $chars = md5(uniqid(mt_rand(), true));
+    $uuid  = substr($chars,0,8) . '-';
+    $uuid .= substr($chars,8,4) . '-';
+    $uuid .= substr($chars,12,4) . '-';
+    $uuid .= substr($chars,16,4) . '-';
+    $uuid .= substr($chars,20,12);
+    return $prefix . $uuid;
+
+  }
   
 }
 
