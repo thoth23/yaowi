d9c8cd08e24fe54c03eb28a62277aea857eaa9a4
alchamist <alchamist@0c2b2200-50f8-11de-96a6-a1d7c9ea8392>
2009-06-25 13:40:50 +0000

    More admin work

--- trunk/index.php
+++ trunk/index.php
@@ -7,7 +7,6 @@
   
   $session = new Session($_REQUEST['login_fname'], $_REQUEST['login_lname'], $_REQUEST['login_password'], $_REQUEST['action'] == 'logoff');
   $osInfo = new OpenSim();
-
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
@@ -17,7 +16,40 @@
 
     <script src="<?php echo $SYSURL; ?>js/prototype.js" type="text/javascript"></script>
     <script src="<?php echo $SYSURL; ?>js/scriptaculous.js" type="text/javascript"></script>
+    
     <script src="map/OpenLayers.js" type="text/javascript"></script>
+
+    <script src="<?php echo $SYSURL; ?>js/tiny_mce/tiny_mce.js" type="text/javascript"></script>
+
+    <script type="text/javascript">
+        tinyMCE.init({
+                // General options
+                mode : "exact",
+                theme : "advanced",
+		elements : "tinyMCE",
+                plugins : "safari,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
+
+                // Theme options
+                theme_advanced_buttons1 : "bold,italic,underline,strikethrough,sub,sup,|,justifyleft,justifycenter,justifyright,justifyfull,fontsizeselect,|,forecolor,backcolor",
+                theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview",
+                theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,charmap,emotions,iespell,media,advhr,|,fullscreen",
+                theme_advanced_toolbar_location : "top",
+                theme_advanced_toolbar_align : "left",
+                theme_advanced_resizing : true,
+
+                // Drop lists for link/image/media/template dialogs
+                template_external_list_url : "lists/template_list.js",
+                external_link_list_url : "lists/link_list.js",
+                external_image_list_url : "lists/image_list.js",
+                media_external_list_url : "lists/media_list.js",
+
+                // Replace values for the template plugin
+                template_replace_values : {
+                        username : "Some User",
+                        staffid : "991234"
+                }
+        });
+    </script>
 
     <?php include($TEMPLATE_PATH . "head.inc.html"); ?>
         
